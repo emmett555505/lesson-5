2deeed59ee924e380aa4a562aad6aebc73cdba74
emmett555505 <edonaldson1122@gmail.com>
2025-07-10 17:31:28 -0400

    lesson 5 project

---
+++ project5.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter first number :"))
+num2 = int(input("enter second number :"))
+num3 = int(input("enter third number :"))
+
+print("your number is %d%d%d" %(num1, num2, num3))
+print("%d%d%d %d%d%d %d%d%d %d%d%d %d%d%d" %(num1,num3,num2, num2,num1,num3, num3,num2,num1, num2,num3,num1, num3,num1,num2))
+
+correct_number = int(input("which of these numbers do you want to swap to :"))
+print("this is your correct number ",correct_number)   
